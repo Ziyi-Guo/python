cd4b518a2756bf54252a8065e44b9bb84b4a98df
Ziyi-Guo <kwokjona@gmail.com>
2016-04-09 14:17:10 +0300

    clipboard:

--- Ziyi-Guo/abundant.cc
+++ Ziyi-Guo/abundant.cc
@@ -1,29 +1,29 @@
 #include <iostream>
 #include "math.h"
-#include "stdlib.h"
 using namespace std;
 
 int main(int argc, char *argv[]){
-	int vol,count=0,num=1;
+	int vol,count=0,num=10;
 
 	if(argc != 2){
 		cout << "Got"<<argc-1<<"argument(s), need one int\n";
 		return -1;
 	}
 	vol = atoi(argv[1]);
-	cout << vol <<endl;
+	// cout << vol <<endl;
 
 	while(count < vol)
 	{
 		num++;
 		double sq =sqrt((double)num);
-		int i,sum=1;
-		for(i=2;i<=(int)sq;i++)
+		int i=2,sum=1;
+		while(i<=(int)sq && sum<=num)
 		{
 			if(num % i == 0)
 			{
 				sum = (i == num/i) ? sum+i : sum+i+num/i;
 			}
+			i++;
 		}
 		if(sum > num)
 		{
